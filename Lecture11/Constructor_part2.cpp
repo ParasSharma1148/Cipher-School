f7e4ce1a8df9f39f5e318166a33fb3c2ae86ee31
ParasSharma1148 <131855704+ParasSharma1148@users.noreply.github.com>
2023-07-14 23:02:27 +0530

    Update Constructor_part2.cpp

--- Lecture11/Constructor_part2.cpp
+++ Lecture11/Constructor_part2.cpp
@@ -1,3 +1,5 @@
+//class_to_basic.cpp
+
 #include<iostream.h>
 using namespace std;
 
@@ -14,14 +16,87 @@ class time{
    void display(){
              cout<<hour<<" "<<min<<endl;
         }
+
+     operator int(){
+         return (this->hour*60)+m;
+     }
+  
+
 };
 
 int main(){
-     int min_from_mid_night = 330;
+     
+   time t(340);
 
-     time t(min_from_mid_night);
-
-     t.display();
+   int time_to_min = (int)t;
+   cout<<"time in mins is"<<time_to_min<<endl;
 
    return 0;
+}
+
+
+
+//class_to_class.cpp
+
+#include<iostream.h>
+using namespace std;
+
+class uk
+{
+  public:
+  int kms;
+  int mts;
+
+   void display()
+   {
+     cout<<kms<<"kms are" <<feet<<"feet"<<endl;
+   } 
+};
+
+class us
+{
+   public:
+   int miles;
+   int feet;
+
+   us(int m,int f)
+   {
+     miles = m;
+     feet = f;
+   }
+
+   us(uk obj)
+   {
+     miles = obj.kms/1,6;
+     feet = obj.mts/0.3;
+   }
+
+   void display()
+   {
+     cout<<miles<<"miles are" <<feet<<"feet"<<endl;
+   } 
+
+   operator uk()
+   {
+     uk duk;
+     duk.km = miles*1.6;
+     duk.mts = feet*0.3;
+
+     return duk;
+
+};
+
+
+int main()
+{
+  us dus(5,10);
+
+  uk duk;
+
+  duk = (uk)dus;
+
+  //us dus2 = duk;
+  us dus2(duk);
+
+  return 0;
 }
